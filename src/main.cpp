a2f9bdde5eca0b3dc4048900ddd3440ca1c4095b
kamranrasul <kamran.rasul@gmail.com>
2020-08-15 23:13:32 -0600

    test commit

--- src/main.cpp
+++ src/main.cpp
@@ -21,15 +21,15 @@
 #define TXD2 17
 
 // Local WiFi Credentials
-#define WIFI_SSID "YOUR SSID"
-#define WIFI_PASS "YOUR PASS"
+#define WIFI_SSID "Hidden_network"
+#define WIFI_PASS "pak.awan.pk"
 
-#define IO_USERNAME "YOUR IO USERNAME"
-#define IO_KEY "YOUR IO KEY"
+#define IO_USERNAME "y238student3"
+#define IO_KEY "aio_EQQT54PN3hqn8qrEJSpVYIzr8awW"
 
 // To send Email using Gmail use port 465 (SSL) and SMTP Server smtp.gmail.com
-#define emailSenderAccount "YOUR SEND EMAIL"
-#define emailSenderPassword "YOUR SENDER EMAIL PASS"
+#define emailSenderAccount "y238student3@gmail.com"
+#define emailSenderPassword "Y238#003"
 
 // email sender initializer
 EMailSender emailSend(emailSenderAccount, emailSenderPassword);
@@ -45,31 +45,45 @@ void updateIO();
 
 AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
 
-// Structure example to receive data
-// Must match the sender structure
+// Must match the sender receiver structure
 typedef struct struct_message
 {
-  int id; // must be unique for each sender board
-  int pinStatus[4];
-  float temperature;
-  float humidity;
-  float pressure;
-  float altitude;
+  int id;            // must be unique for each sender board
+  int pinStatus[4];  // for peripheral status
+  float temperature; // for storing temperature
+  float humidity;    // for storing himmidity
+  float pressure;    // for storing pressure
+  float altitude;    // for storing altitude
+
+  float temp6050;    // for storing onboard temperature
+  float A_values[3]; // for storing accelrometer values
+  float G_values[3]; // for storing gyroscope values
 } struct_message;
 
 // Create an array with all the structures
 struct_message rcdStruct;
 
 // Adafruit.IO feeds
-// Avoid underscores in the feed names, they cause problems with groupings.
-AdafruitIO_Feed *temperatureFeed = io.feed("project.temperature");
-AdafruitIO_Feed *humidityFeed = io.feed("project.humidity");
-AdafruitIO_Feed *barpressureFeed = io.feed("project.barpressure");
-AdafruitIO_Feed *altitudeFeed = io.feed("project.altitude");
-AdafruitIO_Feed *pin14Feed = io.feed("project.pin14");
-AdafruitIO_Feed *pin25Feed = io.feed("project.pin25");
-AdafruitIO_Feed *pin26Feed = io.feed("project.pin26");
-AdafruitIO_Feed *pin27Feed = io.feed("project.pin27");
+// control pin feeds
+AdafruitIO_Feed *pin14Feed = io.feed("controller.pin14");
+AdafruitIO_Feed *pin25Feed = io.feed("controller.pin25");
+AdafruitIO_Feed *pin26Feed = io.feed("controller.pin26");
+AdafruitIO_Feed *pin27Feed = io.feed("controller.pin27");
+
+// BME Sensor feeds
+AdafruitIO_Feed *temperatureFeed = io.feed("sensorBME280.temperature");
+AdafruitIO_Feed *humidityFeed = io.feed("sensorBME280.humidity");
+AdafruitIO_Feed *barpressureFeed = io.feed("sensorBME280.barpressure");
+AdafruitIO_Feed *altitudeFeed = io.feed("sensorBME280.altitude");
+
+// MPU6040 Sensor feeds
+AdafruitIO_Feed *temp = io.feed("sensorMPU6050.temp");
+AdafruitIO_Feed *acx = io.feed("sensorMPU6050.acx");
+AdafruitIO_Feed *acy = io.feed("sensorMPU6050.acy");
+//AdafruitIO_Feed *acz  = io.feed("sensor_MPU6050.acz");
+AdafruitIO_Feed *gyx = io.feed("sensorMPU6050.gyx");
+AdafruitIO_Feed *gyy = io.feed("sensorMPU6050.gyy");
+//AdafruitIO_Feed *gyz  = io.feed("sensor_MPU6050.gyz");
 
 // time variable setup
 const char *ntpServer = "pool.ntp.org";
@@ -86,7 +100,7 @@ int testPin[4] = {0, 0, 0, 0};
 bool firstTime = true;
 
 // scheduled tasks
-Task sendIOUpdate(30000, TASK_FOREVER, &updateIO);
+Task sendIOUpdate(60000, TASK_FOREVER, &updateIO);
 Task emailSender(600000, TASK_FOREVER, &sendEmail);
 
 // Create the scheduler
@@ -130,6 +144,7 @@ void loop()
 // connecting to IO Server
 void connectIO()
 {
+  Serial.printf("Connecting to %s ", WIFI_SSID);
   // Connect to Wifi
   io.connect();
   // wait for a connection
@@ -138,6 +153,7 @@ void connectIO()
     Serial.print(".");
     delay(500);
   }
+  Serial.println(" CONNECTED");
 }
 
 // initializing the tasks
@@ -191,28 +207,32 @@ void sendEmail()
                     String(timeinfo.tm_hour) + ":" +
                     String(timeinfo.tm_min);
 
-  message.message = String("Temperature: ") + String(rcdStruct.temperature) + String(" °C") +
-                    String(" Humidity:    ") + String(rcdStruct.humidity) + String(" %") +
-                    String(" Pressure:    ") + String(rcdStruct.pressure) + String(" hPa") +
-                    String(" Altitude:    ") + String(rcdStruct.altitude) + String(" m") +
-                    String(" Furnace:     ") + String(rcdStruct.pinStatus[0] ? "ON" : "OFF") +
-                    String(" Exhaust:     ") + String(rcdStruct.pinStatus[1] ? "ON" : "OFF") +
-                    String(" Humidifier:  ") + String(rcdStruct.pinStatus[2] ? "ON" : "OFF") +
-                    String(" Light:       ") + String(rcdStruct.pinStatus[3] ? "ON" : "OFF");
+  message.message = String(" *********************_BME280_********************")
+                  + String(" Temperature: \n\n")     + String(rcdStruct.temperature) + String(" °C")
+                  + String(" **************************************************")
+                  + String(" Humidity: ")        + String(rcdStruct.humidity)    + String(" %")
+                  + String(" **************************************************")
+                  + String(" Pressure: ")        + String(rcdStruct.pressure)    + String(" hPa")
+                  + String(" **************************************************")
+                  + String(" Altitude: ")        + String(rcdStruct.altitude)    + String(" m")
+                  + String(" **************************************************")
+                  + String(" Furnace: ")         + String(rcdStruct.pinStatus[0] ? "ON" : "OFF")
+                  + String(" **************************************************")
+                  + String(" Exhaust: ")         + String(rcdStruct.pinStatus[1] ? "ON" : "OFF")
+                  + String(" **************************************************")
+                  + String(" Humidifier: ")      + String(rcdStruct.pinStatus[2] ? "ON" : "OFF")
+                  + String(" **************************************************")
+                  + String(" Light: ")           + String(rcdStruct.pinStatus[3] ? "ON" : "OFF")
+                  + String(" *********************_MPU6050_********************")
+                  + String(" Temperature: ")     + String(rcdStruct.temp6050) + String(" °C")
+                  + String(" **************************************************")
+                  + String(" Acceleration: X: ") + String(rcdStruct.A_values[0]) + ", Y: "+ String(rcdStruct.A_values[1]) + ", Z: " + String(rcdStruct.A_values[2]) + " m/s^2"
+                  + String(" **************************************************")
+                  + String(" Rotation      X: ") + String(rcdStruct.A_values[0]) + ", Y: "+ String(rcdStruct.A_values[1]) + ", Z: " + String(rcdStruct.A_values[2]) + " rad/s";
 
   EMailSender::Response resp;
 
   resp = emailSend.send("kamran.rasul@gmail.com", message);
-
-  Serial.printf("Sending status: %s", resp.status ? "Success" : "Failure");
-  Serial.println();
-
-  resp = emailSend.send("wmalik@ualberta.ca", message);
-
-  Serial.printf("Sending status: %s", resp.status ? "Success" : "Failure");
-  Serial.println();
-
-  resp = emailSend.send("nabil_elassaad@yahoo.com", message);
 
   Serial.printf("Sending status: %s", resp.status ? "Success" : "Failure");
   Serial.println();
@@ -242,8 +262,26 @@ void rcvSerial()
   for (int i = 0; i < 4; i++)
   {
     rcdStruct.pinStatus[i] = Serial2.parseInt();
-    Serial2.read();
     Serial.println(rcdStruct.pinStatus[i]);
+    Serial2.read();
+  }
+
+  rcdStruct.temp6050 = Serial2.parseFloat();
+  Serial.println(rcdStruct.temp6050);
+  Serial2.read();
+
+  for (int i = 0; i < 3; i++)
+  {
+    rcdStruct.A_values[i] = Serial2.parseFloat();
+    Serial.println(rcdStruct.A_values[i]);
+    Serial2.read();
+  }
+
+  for (int i = 0; i < 3; i++)
+  {
+    rcdStruct.G_values[i] = Serial2.parseFloat();
+    Serial.println(rcdStruct.G_values[i]);
+    Serial2.read();
   }
 
   Serial.println("*** Received from Master ***");
@@ -273,13 +311,26 @@ bool detectedChange()
 // update the IO Server
 void updateIO()
 {
+  // updating BME values to IO
   temperatureFeed->save(rcdStruct.temperature);
   humidityFeed->save(rcdStruct.humidity);
   barpressureFeed->save(rcdStruct.pressure);
   altitudeFeed->save(rcdStruct.altitude);
 
+  // updating pin state to IO
   pin14Feed->save(rcdStruct.pinStatus[0]);
   pin25Feed->save(rcdStruct.pinStatus[1]);
   pin26Feed->save(rcdStruct.pinStatus[2]);
   pin27Feed->save(rcdStruct.pinStatus[3]);
+
+  // updating MPU6050 values to IO
+  temp->save(rcdStruct.temp6050);
+  acx->save(rcdStruct.A_values[0]);
+  acy->save(rcdStruct.A_values[1]);
+  //acz->save(rcdStruct.A_values[2]);
+  gyx->save(rcdStruct.G_values[0]);
+  gyx->save(rcdStruct.G_values[1]);
+  //gyx->save(rcdStruct.G_values[2]);
+
+  Serial.println("IO Feed updated...");
 }
