6bd64b84c586d83892563663df610bb9680e80e5
kamranrasul <kamran.rasul@gmail.com>
2020-08-17 16:20:04 -0600

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-/*********
+/************
   
   Kamran R.
   Waqas M.
@@ -7,7 +7,7 @@
   Robbin Law
   Rui Santos
   
-*********/
+************/
 
 #include <Arduino.h>          // base arduino library
 #include "ESP32_MailClient.h" // for email
@@ -46,12 +46,12 @@ void sendCallback(SendStatus info);
 
 // fuctions for operation
 void taskInitilizer();
-void connectIO();
 void printLocalTime();
-void sendEmail();
+void connectIO();
 void rcvSerial();
-bool detectedChange();
+void sendEmail();
 void updateIO();
+bool detectedChange();
 
 AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
 
@@ -59,15 +59,20 @@ AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
 typedef struct struct_message
 {
   int id;            // must be unique for each sender board
+
+  // controller pin state
   int pinStatus[4];  // for peripheral status
+
+  // for BME Chip
   float temperature; // for storing temperature
   float humidity;    // for storing himmidity
   float pressure;    // for storing pressure
   float altitude;    // for storing altitude
 
+  // for MPU Chip
   float temp6050;    // for storing onboard temperature
-  float A_values[3]; // for storing accelrometer values
-  float G_values[3]; // for storing gyroscope values
+  float A_values[3]; // for storing accelrometer values x, y, z
+  float G_values[3]; // for storing gyroscope values x, y, z
 } struct_message;
 
 // Create an array with all the structures
@@ -82,17 +87,17 @@ AdafruitIO_Feed *pin27Feed = io.feed("controller.pin27");
 
 // BME Sensor feeds
 AdafruitIO_Feed *temperatureFeed = io.feed("sensorBME280.temperature");
-AdafruitIO_Feed *humidityFeed = io.feed("sensorBME280.humidity");
+AdafruitIO_Feed *humidityFeed    = io.feed("sensorBME280.humidity");
 AdafruitIO_Feed *barpressureFeed = io.feed("sensorBME280.barpressure");
-AdafruitIO_Feed *altitudeFeed = io.feed("sensorBME280.altitude");
+AdafruitIO_Feed *altitudeFeed    = io.feed("sensorBME280.altitude");
 
 // MPU6040 Sensor feeds
-AdafruitIO_Feed *temp = io.feed("sensorMPU6050.temp");
-AdafruitIO_Feed *acx = io.feed("sensorMPU6050.acx");
-AdafruitIO_Feed *acy = io.feed("sensorMPU6050.acy");
+AdafruitIO_Feed *temp   = io.feed("sensorMPU6050.temp");
+AdafruitIO_Feed *acx    = io.feed("sensorMPU6050.acx");
+//AdafruitIO_Feed *acy    = io.feed("sensorMPU6050.acy");
 //AdafruitIO_Feed *acz  = io.feed("sensor_MPU6050.acz");
-AdafruitIO_Feed *gyx = io.feed("sensorMPU6050.gyx");
-AdafruitIO_Feed *gyy = io.feed("sensorMPU6050.gyy");
+AdafruitIO_Feed *gyx    = io.feed("sensorMPU6050.gyx");
+//AdafruitIO_Feed *gyy    = io.feed("sensorMPU6050.gyy");
 //AdafruitIO_Feed *gyz  = io.feed("sensor_MPU6050.gyz");
 
 // time variable setup
@@ -103,7 +108,7 @@ const int daylightOffset_sec = 3600;
 // time variable
 struct tm timeinfo;
 
-// test pins and initialize them to 0
+// test pins last status
 int testPin[4] = {0, 0, 0, 0};
 
 // enabling email server once the update recieves
@@ -136,10 +141,17 @@ void loop()
 
   if (Serial2.available())
   {
+    // call the data recieve function
     rcvSerial();
+
+    // run at first data recieved
     if (firstTime)
     {
+      // enable the task
       emailSender.enable();
+      sendIOUpdate.enable();
+      //runner.enableAll();
+
       firstTime = false;
     }
   }
@@ -155,8 +167,10 @@ void loop()
 void connectIO()
 {
   Serial.printf("Connecting to %s ", WIFI_SSID);
+
   // Connect to Wifi
   io.connect();
+
   // wait for a connection
   while (io.status() < AIO_CONNECTED)
   {
@@ -181,9 +195,6 @@ void taskInitilizer()
   // Add the task to the scheduler
   runner.addTask(sendIOUpdate);
   runner.addTask(emailSender);
-
-  // enable the task
-  sendIOUpdate.enable();
 }
 
 // time function
@@ -195,65 +206,6 @@ void printLocalTime()
     return;
   }
   Serial.println(&timeinfo, "\n%A, %B %d, %Y %H:%M:%S");
-}
-
-// email send function
-void sendEmail()
-{
-    // printing at Serial Port
-  Serial.println();
-  Serial.println("Preparing to send email");
-  Serial.println();
-
-  // setting up subject according to current date and time
-  String emailSubject = "ESP32 HA Update on "
-                      + String(timeinfo.tm_year + 1900) + "-"
-                      + String(timeinfo.tm_mon + 1) + "-"
-                      + String(timeinfo.tm_mday) + " at "
-                      + String(timeinfo.tm_hour) + ":"
-                      + String(timeinfo.tm_min)  + " hours";
-
-  String emailMessage = "*** Controller Pin State ***"
-                        + String("\nFurnace: ")       + (rcdStruct.pinStatus[0] ? "ON" : "OFF")
-                        + String("\nExhaust: ")       + (rcdStruct.pinStatus[1] ? "ON" : "OFF")
-                        + String("\nHumidifier: ")    + (rcdStruct.pinStatus[2] ? "ON" : "OFF")
-                        + String("\nLight: ")         + (rcdStruct.pinStatus[3] ? "ON" : "OFF")
-                        + String("\n\n*** Values from BME280 ***")
-                        + String("\nTemperature: ")   + String(rcdStruct.temperature)  + " °C"
-                        + String("\nHumidity: ")      + String(rcdStruct.humidity)     + " %"
-                        + String("\nPressure: ")      + String(rcdStruct.pressure)     + " hPa"
-                        + String("\nAltitude: ")      + String(rcdStruct.altitude)     + " m"
-                        + String("\n\n*** Values from MPU6050 ***")
-                        + String("\nTemperature: ")   + String(rcdStruct.temp6050)     + " °C"
-                        + String("\nAccelration X: ") + String(rcdStruct.A_values[0])  + ", Y: " + String(rcdStruct.A_values[1]) + ", Z: " + String(rcdStruct.A_values[2])
-                        + String("\nRotation X: ")    + String(rcdStruct.G_values[0])  + ", Y: " + String(rcdStruct.G_values[1]) + ", Z: " + String(rcdStruct.G_values[2]);
-
-  // Set the SMTP Server Email host, port, account and password
-  smtpData.setLogin(smtpServer, smtpServerPort, emailSenderAccount, emailSenderPassword);
-
-  // Set the sender name and Email
-  smtpData.setSender("ESP32", emailSenderAccount);
-
-  // Set Email priority or importance High, Normal, Low or 1 to 5 (1 is highest)
-  smtpData.setPriority("High");
-
-  // Set the subject
-  smtpData.setSubject(emailSubject);
-
-  // Set the message with HTML format
-  smtpData.setMessage(emailMessage, false);
-
-  // Add recipients, you can add more than one recipient
-  smtpData.addRecipient(emailRecipient);
-
-  smtpData.setSendCallback(sendCallback);
-
-  //Start sending Email, can be set callback function to track the status
-  if (!MailClient.sendMail(smtpData))
-    Serial.println("Error sending Email, " + MailClient.smtpErrorReason());
-
-  //Clear all data from Email object to free memory
-  smtpData.empty();
 }
 
 // for receiving data from Serial port
@@ -302,28 +254,71 @@ void rcvSerial()
     Serial2.read();
   }
 
+  Serial2.println(200);
+
   Serial.println("*** Received from Master ***");
   Serial.println();
 }
 
-// check if there is any update on the pins
-bool detectedChange()
+// email send function
+void sendEmail()
 {
-  for (int i = 0; i < 4; i++)
+  // printing at Serial Port
+  Serial.println();
+  Serial.println("Preparing to send email..");
+  Serial.println();
+
+  // setting up subject according to current date and time
+  String emailSubject = "ESP32 HA Update on "
+                      + String(timeinfo.tm_year + 1900) + "-"
+                      + String(timeinfo.tm_mon + 1) + "-"
+                      + String(timeinfo.tm_mday) + " at "
+                      + String(timeinfo.tm_hour) + ":"
+                      + String(timeinfo.tm_min)  + " hours";
+
+  String emailMessage = "\n*** Controller Pin State ***"
+                        + String("\nFurnace: ")       + (rcdStruct.pinStatus[0] ? "ON" : "OFF")
+                        + String("\nExhaust: ")       + (rcdStruct.pinStatus[1] ? "ON" : "OFF")
+                        + String("\nHumidifier: ")    + (rcdStruct.pinStatus[2] ? "ON" : "OFF")
+                        + String("\nLight: ")         + (rcdStruct.pinStatus[3] ? "ON" : "OFF")
+                        + String("\n\n*** Values from BME280 ***")
+                        + String("\nTemperature: ")   + String(rcdStruct.temperature)  + " °C"
+                        + String("\nHumidity: ")      + String(rcdStruct.humidity)     + " %"
+                        + String("\nPressure: ")      + String(rcdStruct.pressure)     + " hPa"
+                        + String("\nAltitude: ")      + String(rcdStruct.altitude)     + " m"
+                        + String("\n\n*** Values from MPU6050 ***")
+                        + String("\nTemperature: ")   + String(rcdStruct.temp6050)     + " °C"
+                        + String("\nAccelration X: ") + String(rcdStruct.A_values[0])  + ", Y: " + String(rcdStruct.A_values[1]) + ", Z: " + String(rcdStruct.A_values[2])
+                        + String("\nRotation X: ")    + String(rcdStruct.G_values[0])  + ", Y: " + String(rcdStruct.G_values[1]) + ", Z: " + String(rcdStruct.G_values[2]);
+
+  // Set the SMTP Server Email host, port, account and password
+  smtpData.setLogin(smtpServer, smtpServerPort, emailSenderAccount, emailSenderPassword);
+
+  // Set the sender name and email
+  smtpData.setSender("ESP32", emailSenderAccount);
+
+  // Set Email priority or importance High, Normal, Low or 1 to 5 (1 is highest)
+  smtpData.setPriority("High");
+
+  // Set the subject
+  smtpData.setSubject(emailSubject);
+
+  // Set the message with simple format
+  smtpData.setMessage(emailMessage, false);
+
+  // Add recipients, you can add more than one recipient
+  smtpData.addRecipient(emailRecipient);
+
+  smtpData.setSendCallback(sendCallback);
+
+  //Start sending Email, can be set callback function to track the status
+  if (!MailClient.sendMail(smtpData))
   {
-    if (testPin[i] != rcdStruct.pinStatus[i])
-    {
-      // if there is change then update
-      for (int j = 0; j < 4; j++)
-      {
-        testPin[j] = rcdStruct.pinStatus[j];
-      }
-      // return change flag
-      return true;
-    }
+    Serial.println("Error sending Email, " + MailClient.smtpErrorReason());
   }
-  // return no change flag
-  return false;
+
+  //Clear all data from Email object to free memory
+  smtpData.empty();
 }
 
 // Callback function to get the Email sending status
@@ -357,11 +352,31 @@ void updateIO()
   // updating MPU6050 values to IO
   temp->save(rcdStruct.temp6050);
   acx->save(rcdStruct.A_values[0]);
-  acy->save(rcdStruct.A_values[1]);
+  //acy->save(rcdStruct.A_values[1]);
   //acz->save(rcdStruct.A_values[2]);
   gyx->save(rcdStruct.G_values[0]);
-  gyx->save(rcdStruct.G_values[1]);
+  //gyx->save(rcdStruct.G_values[1]);
   //gyx->save(rcdStruct.G_values[2]);
 
   Serial.println("IO Feed updated...");
+}
+
+// check if there is any update on the pins
+bool detectedChange()
+{
+  for (int i = 0; i < 4; i++)
+  {
+    if (testPin[i] != rcdStruct.pinStatus[i])
+    {
+      // if there is change then update
+      for (int j = 0; j < 4; j++)
+      {
+        testPin[j] = rcdStruct.pinStatus[j];
+      }
+      // return change flag
+      return true;
+    }
+  }
+  // return no change flag
+  return false;
 }
